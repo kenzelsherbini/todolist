3b59849d0a411373805f0d5b758a369a914e5ee3
kenzelsherbini <526kelsherbini@frhsd.com>
2025-02-24 10:36:36 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+//innit an array to store tasks 
+let tasks = [""]
+//add an event listener to add tasks 
+document.getElementById("addTaskBtn").addEventListener('click', function() {
+//storing text value from input as a variable task input 
+    let taskInput = document.getElementById('taskInput').value
+//checks if anything is in the input box
+    if (taskInput){
+//adds the new task
+    tasks.push(taskInput)
+//clears what is in the input box 
+    document.getElementById('taskInput').value = ''
+
+    displayTasks()
+    }
+})
